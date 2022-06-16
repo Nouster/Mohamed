773963b023c93e98418b139e7117e876f3783473
Nouster <djebali.mohamed88@gmail.com>
2022-06-16 18:54:03 +0200

    Initial Commit

---
+++ Mohamed/MohamedApp.swift
@@ -0,0 +1,17 @@
+//
+//  MohamedApp.swift
+//  Mohamed
+//
+//  Created by Mohamed Djebali on 16/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct MohamedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
